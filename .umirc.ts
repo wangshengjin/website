2d23ea0f9a155863f0c6bc7c0e088987c34fa586
wangshengjin <934457719@qq.com>
2024-12-06 23:34:30 +0800

    history-> hash

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'umi';
 export default defineConfig({
 	outputPath: 'dist',
 	hash: true,
+	history: { type: 'hash' },
 	copy: [
 		{ from: 'app-ads.txt', to: 'dist/app-ads.txt' },
 	]
